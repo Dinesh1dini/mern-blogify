7abf8668cc18e906cb35ffe99531acbff87e6258
Dinesh1dini <dinidinesh497@example.com>
2025-01-30 01:42:58 -0800

    server

---
+++ server.js
@@ -0,0 +1,12 @@
+const http = require("http");
+const express =require("express");
+
+
+const app = express();
+
+const server = http.createServer(app);
+
+
+
+const PORT = process.env.PORT || 9080;
+server.listen(PORT,console.log(`server running on port ${PORT}`));
